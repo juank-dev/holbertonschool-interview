1b98a950c21ab0aad353d7469eadc63aad3e6ceb
Juan <1213@holbertonschool.com>
2020-07-21 19:14:23 +0000

    Correction point 0

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -4,19 +4,19 @@
 def canUnlockAll(boxes):
     """Lockboxes Holberton Interview"""
     num_box = 0
-    keys = {0}
+    keys = [0]
     if boxes is None or boxes == []:
         return False
     for x in boxes:
         if num_box not in keys:
             return False
         for y in x:
-            keys.add(y)
-        num = 0
-        for w in boxes:
-            if num in keys:
-                for z in w:
-                    keys.add(z)
-            num += 1
+            if y < len(boxes):
+                keys.append(y)
+        for w in keys:
+            for z in boxes[w]:
+                if z not in keys:
+                    if z < len(boxes):
+                        keys.append(z)
         num_box += 1
     return True
