d60d5704fc2a8e9ee56b78ce180be3ace1139f6c
Juan <1213@holbertonschool.com>
2020-08-12 02:52:25 +0000

    [feature] Minimum operation correction

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -8,25 +8,19 @@ a number n, write a method that calculates the fewest number of operations
 
 
 def minOperations(n):
-    """function that validate the number of Minimum Operations"""
-    copy = 1
+    """ Calculates the fewest number of operations needed
+        to result in exactly n H characters in the file
+    """
     count = 0
-    minimo = []
-    if n < 2:
-        return 0
-    for paste in range(1, n):
-        count = 0
-        copy = 1
-        for number in range(n):
-            if n == copy:
-                minimo.append(count)
-                break
-            if copy > n:
-                count -= paste + 1
-                copy -= copy/(paste + 1) * paste
-                copy += paste
-                count += 1
-                continue
-            copy += copy * paste
-            count += paste + 1
-    return(min(minimo))
+    copy = 1
+    tmp = 0
+
+    while copy < n:
+        if n % copy == 0:
+            tmp = copy
+            copy *= 2
+            count += 1
+        else:
+            copy += tmp
+        count += 1
+    return count
