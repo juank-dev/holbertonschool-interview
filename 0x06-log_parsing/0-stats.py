3bd6497fb08390cd592ddf88680bc686747dd78b
Juan <1213@holbertonschool.com>
2020-09-03 01:36:25 +0000

    Log Parsing

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 import sys
+import signal
+
 
 size, count = 0, 1
 var200, var301, var400, var401 = 0, 0, 0, 0
@@ -7,17 +9,34 @@ var403, var404, var405, var500 = 0, 0, 0, 0
 my_dict = {"200": var200, "301": var301, "400": var400, "401": var401,
            "403": var403, "404": var404, "405": var405, "500": var500}
 
-for line in sys.stdin:
-    word = line.split()
-    size += int(word[-1])
-    if count == 10:
-        print("File size: {}".format(size))
-        for status, variable in my_dict.items():
-            if variable != 0:
-                print("{}: {}".format(status, variable))
-        count = 1
-    for status in my_dict.keys():
-        if (word[-2] == status):
-            my_dict[status] += 1
+"""
+def keyboardInterruptHandler(signal, frame):
+    print("KeyboardInterrupt (ID: {}) has been caught. Cleaning up...".format(signal))
+    print(my_dyct["200"])
+    return(1)
 
-    count += 1
+
+signal.signal(signal.SIGINT, keyboardInterruptHandler)
+
+"""
+try:
+    for line in sys.stdin:
+        word = line.split()
+        size += int(word[-1])
+        if count == 10:
+            print("File size: {}".format(size))
+            for status, variable in my_dict.items():
+                if variable != 0:
+                    print("{}: {}".format(status, variable))
+            count = 1
+        for status in my_dict.keys():
+            if (word[-2] == status):
+                my_dict[status] += 1
+
+        count += 1
+except KeyboardInterrupt:
+    print("File size: {}".format(size))
+    for status, variable in my_dict.items():
+        if variable != 0:
+            print("{}: {}".format(status, variable))
+    sys.exit()
