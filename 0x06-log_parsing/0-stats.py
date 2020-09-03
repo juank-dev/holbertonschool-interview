6b8063e452b147a2b9d1423f3581ccaf8bef9e6b
Juan <1213@holbertonschool.com>
2020-09-03 19:23:34 +0000

    [Feature] exception wrong format

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -11,6 +11,14 @@ if __name__ == '__main__':
     my_dict = {"500": 0, "301": 0, "400": 0, "401": 0,
                "403": 0, "404": 0, "405": 0, "200": 0}
 
+    def print_stadistics(size, my_dict):
+        """print metrics"""
+        print("File size: {}".format(size))
+        od = collections.OrderedDict(sorted(my_dict.items()))
+        for status, variable in od.items():
+            if variable != 0:
+                print("{}: {}".format(status, variable))
+
     try:
         """Validation computes metrics and print stadistics"""
         for line in sys.stdin:
@@ -21,25 +29,14 @@ if __name__ == '__main__':
                     if (word[-2] == status):
                         my_dict[status] += 1
                 if count == 10:
-                    print("File size: {}".format(size))
-                    od = collections.OrderedDict(sorted(my_dict.items()))
-                    for status, variable in od.items():
-                        if variable != 0:
-                            print("{}: {}".format(status, variable))
+                    print_stadistics(size, my_dict)
                     count = 0
             except:
                 pass
             count += 1
     except KeyboardInterrupt:
         """Keyboard interrupt"""
-        print("File size: {}".format(size))
-        od = collections.OrderedDict(sorted(my_dict.items()))
-        for status, variable in od.items():
-            if variable != 0:
-                print("{}: {}".format(status, variable))
-        raise
-    print("File size: {}".format(size))
-    od = collections.OrderedDict(sorted(my_dict.items()))
-    for status, variable in od.items():
-        if variable != 0:
-            print("{}: {}".format(status, variable))
+        print_stadistics(size, my_dict)
+        exit(0)
+
+    print_stadistics(size, my_dict)
