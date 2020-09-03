00a86c808ef43df06e6844463643eff5e7a65097
Juan <1213@holbertonschool.com>
2020-09-03 03:05:13 +0000

    Log Parsing

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -6,10 +6,9 @@ if __name__ == '__main__':
     import sys
 
     size, count = 0, 0
-    var200, var301, var400, var401 = 0, 0, 0, 0
-    var403, var404, var405, var500 = 0, 0, 0, 0
-    my_dict = {"200": var200, "301": var301, "400": var400, "401": var401,
-               "403": var403, "404": var404, "405": var405, "500": var500}
+
+    my_dict = {"200": 0, "301": 0, "400": 0, "401": 0,
+               "403": 0, "404": 0, "405": 0, "500": 0}
 
     try:
         """Validation computes metrics and print stadistics"""
