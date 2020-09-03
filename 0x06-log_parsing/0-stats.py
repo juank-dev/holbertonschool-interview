abdc400634cacc28a5b7eb02598464f0ec14ff56
Juan <1213@holbertonschool.com>
2020-09-03 01:55:07 +0000

    Log Parsing

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""Log parsing Write a script that reads stdin line by line and computes metrics"""
+"""Log parsing Write a script that reads stdin line by
+    line and computes metrics"""
 
 import sys
 import signal
