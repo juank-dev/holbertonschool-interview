ea45c76a65401f55617e7b8787200e1a630a9ee0
Juan <1213@holbertonschool.com>
2020-09-03 01:13:25 +0000

    Log Parsing

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -1,22 +1,11 @@
 #!/usr/bin/python3
 import sys
-from signal import signal, SIGINT
-
-
-def handler(signal_received, frame):
-    """Signal"""
-    print('SIGINT or CTRL-C detected. Exiting gracefully')
-    exit(0)
-
-
-signal(SIGINT, handler)
 
 size, count = 0, 1
 var200, var301, var400, var401 = 0, 0, 0, 0
 var403, var404, var405, var500 = 0, 0, 0, 0
 my_dict = {"200": var200, "301": var301, "400": var400, "401": var401,
            "403": var403, "404": var404, "405": var405, "500": var500}
-
 
 for line in sys.stdin:
     word = line.split()
