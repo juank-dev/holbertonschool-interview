e8248d1fad908f738488713931aec7bac355b235
Juan <1213@holbertonschool.com>
2020-09-12 13:20:01 -0500

    prueba

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -11,14 +11,6 @@ if __name__ == '__main__':
     my_dict = {"500": 0, "301": 0, "400": 0, "401": 0,
                "403": 0, "404": 0, "405": 0, "200": 0}
 
-    def print_stadistics(size, my_dict):
-        """print metrics"""
-        print("File size: {}".format(size))
-        od = collections.OrderedDict(sorted(my_dict.items()))
-        for status, variable in od.items():
-            if variable != 0:
-                print("{}: {}".format(status, variable))
-
     try:
         """Validation computes metrics and print stadistics"""
         for line in sys.stdin:
@@ -29,14 +21,19 @@ if __name__ == '__main__':
                     if (word[-2] == status):
                         my_dict[status] += 1
                 if count == 10:
-                    print_stadistics(size, my_dict)
+                    print("File size: {}".format(size))
+                    od = collections.OrderedDict(sorted(my_dict.items()))
+                    for status, variable in od.items():
+                        if variable != 0:
+                print("{}: {}".format(status, variable))
+
                     count = 0
             except:
                 pass
             count += 1
     except KeyboardInterrupt:
         """Keyboard interrupt"""
-        print_stadistics(size, my_dict)
-        exit(0)
-
-    print_stadistics(size, my_dict)
+        print("File size: {}".format(size))
+        od = collections.OrderedDict(sorted(my_dict.items()))
+        for status, variable in od.items():
+            if variable != 0:            
