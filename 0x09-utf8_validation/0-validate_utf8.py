87e37783e1946bdd09fa93554693c3f2cb8a1b8a
Juan <1213@holbertonschool.com>
2020-09-24 15:00:10 -0500

    UTF-8 VAlidate

---
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""
+Validate UTF-8
+"""
+
+
+def validUTF8(data):
+    """
+    validate UTF-8
+    """
+    for number in range(len(data)):
+        tmp_num = data[number]
+        tmp = tmp_num >> 7
+
+        if (int("{0:b}".format(tmp)) == 0):
+            continue
+        tmp = tmp_num >> 5
+        if(int("{0:b}".format(tmp)) == 110):
+            tmp = data[number + 1] >> 6
+            if(int("{0:b}".format(tmp)) == 10):
+                number += 1
+                continue
+            else:
+                return('False')
+        tmp = tmp_num >> 4
+        if(int("{0:b}".format(tmp)) == 1110):
+            tmp = data[number + 1] >> 6
+            tmp_2 = data[number + 2] >> 6
+            if int("{0:b}".format(tmp)) == 10 and int("{0:b}".format(tmp_2)) == 10:
+                number += 2
+                continue
+            else:
+                return('False')
+        tmp = tmp_num >> 3
+        if(int("{0:b}".format(tmp)) == 11110):
+            tmp = data[number + 1] >> 6
+            tmp_2 = data[number + 2] >> 6
+            tmp_3 = data[number + 2] >> 6
+            if int("{0:b}".format(tmp)) == 10 and int("{0:b}".format(tmp_2)) == 10 and int("{0:b}".format(tmp_3)) == 10:
+                number += 3
+                continue
+            else:
+                return('False')
+    return('True')
