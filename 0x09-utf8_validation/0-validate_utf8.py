917968562731de887132608f9ccb8dce97edb1f0
Juan <1213@holbertonschool.com>
2020-09-24 15:44:19 -0500

    UTF-8 VAlidate

--- 0x09-utf8_validation/0-validate_utf8.py
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -4,6 +4,21 @@ Validate UTF-8
 """
 
 
+def validation(data, number, tmp, bytes):
+    tmp_list = []
+    try:
+        for x in range(bytes):
+            tmp_list.append(int("{0:b}".format(data[number + (x + 1)] >> 6)))
+
+        for val in tmp_list:
+            if val != 10:
+                return(0)
+        number += bytes
+        return(number)
+    except:
+        return(-1)
+
+
 def validUTF8(data):
     """
     validate UTF-8
@@ -16,30 +31,23 @@ def validUTF8(data):
             continue
         tmp = tmp_num >> 5
         if(int("{0:b}".format(tmp)) == 110):
-            tmp = data[number + 1] >> 6
-            if(int("{0:b}".format(tmp)) == 10):
-                number += 1
-                continue
-            else:
+            number = validation(data, number, tmp, 1)
+            if number == -1 or number == 0:
                 return('False')
+            continue
+
         tmp = tmp_num >> 4
         if(int("{0:b}".format(tmp)) == 1110):
-            tmp = int("{0:b}".format(data[number + 1] >> 6))
-            tmp_2 = int("{0:b}".format(data[number + 2] >> 6))
-            if tmp == 10 and tmp_2 == 10:
-                number += 2
-                continue
-            else:
+            number = validation(data, number, tmp, 2)
+            if number == -1 or number == 0:
                 return('False')
+            continue
+
         tmp = tmp_num >> 3
         if(int("{0:b}".format(tmp)) == 11110):
-            tmp = int("{0:b}".format(data[number + 1] >> 6))
-            tmp_2 = int("{0:b}".format(data[number + 2] >> 6))
-            tmp_3 = int("{0:b}".format(data[number + 3] >> 6))
-
-            if tmp == 10 and tmp_2 == 10 and tmp_3 == 10:
-                number += 3
-                continue
-            else:
+            number = validation(data, number, tmp, 3)
+            if number == -1 or number == 0:
                 return('False')
+            continue
+
     return('True')
