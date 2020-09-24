cf0c9af7d24f4797759076ad8bd580cf2a2db1c8
Juan <1213@holbertonschool.com>
2020-09-24 16:51:28 -0500

    UTF-8 VAlidate

--- 0x09-utf8_validation/0-validate_utf8.py
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -14,8 +14,7 @@ def validation(data, number, bytes):
         for val in tmp_list:
             if val != 10:
                 return(0)
-        number += bytes
-        return(number)
+        return(bytes)
     except IndexError:
         return(-1)
 
@@ -24,32 +23,37 @@ def validUTF8(data):
     """
     validate UTF-8
     """
-
+    step = 0
     for number in range(len(data)):
+        if step:
+            step -= 1
+            continue
+
         tmp_num = data[number] & 255
         tmp = tmp_num >> 7
 
         if (int("{0:b}".format(tmp)) == 0):
             continue
+
         tmp = tmp_num >> 5
         if(int("{0:b}".format(tmp)) == 110):
-            number = validation(data, number, 1)
-            if number == -1 or number == 0:
+            step = validation(data, number, 1)
+            if step == -1 or step == 0:
                 return('False')
             continue
 
         tmp = tmp_num >> 4
         if(int("{0:b}".format(tmp)) == 1110):
-            number = validation(data, number, 2)
-            if number == -1 or number == 0:
+            step = validation(data, number, 2)
+            if step == -1 or step == 0:
                 return('False')
             continue
 
         tmp = tmp_num >> 3
         if(int("{0:b}".format(tmp)) == 11110):
-            number = validation(data, number, 3)
-            if number == -1 or number == 0:
+            step = validation(data, number, 3)
+            if step == -1 or step == 0:
                 return('False')
             continue
-
+        return('False')
     return('True')
