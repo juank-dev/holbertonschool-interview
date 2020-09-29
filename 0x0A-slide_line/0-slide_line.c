66de3b8a626ec13bab22fb603cc78a890af15043
Juan <1213@holbertonschool.com>
2020-09-29 12:39:04 -0500

    [feature] Slide Line

--- 0x0A-slide_line/0-slide_line.c
+++ 0x0A-slide_line/0-slide_line.c
@@ -1,46 +1,50 @@
-#include <stdlib.h>
-#include <stdio.h>
-
 #include "slide_line.h"
 
 
+/**
+ * slide_line - Prints out an array of integer, followed by a new line
+ * @line: Pointer to the array of integer to be printed
+ * @size: Number of elements in @array
+ * @direction: direction
+ * Return: EXIT_SUCCESS or EXIT_FAILURE
+ */
 int slide_line(int *line, size_t size, int direction)
 {
-    int list[size], i = 0, j= 0, k = 0, num = 0, size_n = size;
+	int list[size], i = 0, j = 0, k = 0, num = 0, size_n = size;
 
-    if(direction == 0)
-    {
-        for (i = 0; i < size_n; i++)
-        {
-            if(line[i] != 0)
-                list[j++] = line[i];
-            line[i] = 0;
-        }
-        for(i = 0; i < j; i++)
-        {
-            if (list[i] == list[i+1])
-                line[k++] = list[i] + list[i+1], i++;
-            else if(list[i] != 0)
-                line[k++] = list[i];
-        }
-    }
-    else
-    {
-        for (i = size - 1; i > -1; i--)
-        {
-            if(line[i] != 0)
-                list[j++] = line[i];
-            line[i] = 0;
-        }
-        k = size - 1, num = j;
+	if (direction == 0)
+	{
+		for (i = 0; i < size_n; i++)
+		{
+			if (line[i] != 0)
+				list[j++] = line[i];
+			line[i] = 0;
+		}
+		for (i = 0; i < j; i++)
+		{
+			if (list[i] == list[i + 1])
+				line[k++] = list[i] + list[i + 1], i++;
+			else if (list[i] != 0)
+				line[k++] = list[i];
+		}
+	}
+	else
+	{
+		for (i = size - 1; i > -1; i--)
+		{
+			if (line[i] != 0)
+				list[j++] = line[i];
+			line[i] = 0;
+		}
+		k = size - 1, num = j;
 
-        for(i = 0; i < num; i++)
-        {   
-            if (list[i] == list[i+1])
-                line[k--] = list[i] + list[i+1], i++;
-            else if(list[i] != 0)
-                line[k--] = list[i];
-        }  
-    }
-    return(1);
+		for (i = 0; i < num; i++)
+		{
+			if (list[i] == list[i + 1])
+				line[k--] = list[i] + list[i + 1], i++;
+			else if (list[i] != 0)
+				line[k--] = list[i];
+		}
+	}
+	return (1);
 }
--- 0x0A-slide_line/slide_line.h
+++ 0x0A-slide_line/slide_line.h
@@ -1,6 +1,9 @@
 #ifndef SLIDE_LINE
 #define SLIDE_LINE
 
+#include <stdlib.h>
+#include <stdio.h>
+
 #define SLIDE_LEFT  0
 #define SLIDE_RIGHT  1
 int slide_line(int *line, size_t size, int direction);
