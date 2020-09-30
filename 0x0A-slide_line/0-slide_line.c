019e7cbbfce99b1baae6baba7621b795f07653d1
Juan <1213@holbertonschool.com>
2020-09-29 23:05:47 -0500

    [feature] slide line

--- 0x0A-slide_line/0-slide_line.c
+++ 0x0A-slide_line/0-slide_line.c
@@ -13,7 +13,7 @@
 int slide_line(int *line, size_t size, int direction)
 {
 
-	int i = 0, j = 0, k = 0, num = 0, size_n = size, data = 0;
+	int i = 0, k = 0, size_n = size, data = 0;
 
 	if (line == NULL || (direction != 0 && direction != 1))
 		return (0);
@@ -40,27 +40,31 @@ int slide_line(int *line, size_t size, int direction)
 			}
 		}
 		for (i = k; i < size_n; i++)
-		{
 			line[i] = 0;
-		}
-
 	}
 	else
 	{
-		for (i = size - 1; i > -1; i--)
+		k = size - 1;
+		for (i = size_n - 1; i > -1; i--)
 		{
-			if (line[i] != 0)
-				line[j++] = line[i];
-			line[i] = 0;
-		}
-		k = size - 1, num = j;
-
-		for (i = 0; i < num; i++)
-		{
-			if (line[i] == line[i + 1])
-				line[k--] = line[i] + line[i + 1], i++;
+			if (line[i] == line[i - 1]  && line[i] != 0)
+				{
+					line[k--] = line[i] + line[i - 1], i--;
+					line[k] = 0;
+				}
 			else if (line[i] != 0)
-				line[k--] = line[i];
+			{
+				if(data == line[i])
+				{
+					line[++k] = data * 2;
+					k--;
+					data = 0;
+				}
+				else
+					line[k--] = line[i], data = line[i];
+			}
 		}
+		for (i = k; i > -1; i--)
+			line[i] = 0;
 	}	return (1);
 }
