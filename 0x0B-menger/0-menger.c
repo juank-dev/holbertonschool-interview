35eb546ee0394e629fd2801bf93ce15c506c39e6
Juan <1213@holbertonschool.com>
2020-10-07 19:47:17 -0500

    [feature] Menger Sponge

--- 0x0B-menger/0-menger.c
+++ 0x0B-menger/0-menger.c
@@ -10,7 +10,7 @@
 
 void menger(int level)
 {
-	int i, j, k, pot_a, flag = 0;
+	int i, j, pot_a;
 
 	if (level < 0)
 		return;
@@ -21,8 +21,6 @@ void menger(int level)
 	{
 		for (j = 0; j < pot_a; j++)
 		{
-			flag = 0;
-
 			if (i % 3 == 1 && j % 3 == 1)
 			{
 				printf(" ");
@@ -38,39 +36,68 @@ void menger(int level)
 				}
 			}
 
-			for (k = 1; k < pot_a / 3; k += 3)
-			{
-				for (int l = 1; l < pot_a / 3; l += 3)
-				{
-					if ((pot_a / 9) * k <= i && ((pot_a / 9) * k) + pot_a / 9 > i)
-					{
-						if ((pot_a / 9) * l <= j && ((pot_a / 9) * l) + pot_a / 9 > j)
-						{
-							printf(" ");
-							flag = 1;
-						}
-					}
-				}
-			}
-			if (flag)
+			if (validate_1(pot_a, i, j))
 				continue;
-			for (k = 3; k < 6; k++)
-			{
-				for (int l = 3; l < 6; l++)
-				{
-					if (i % 9 == k && j % 9 == l)
-					{
-						printf(" ");
-						flag = 1;
-						continue;
-					}
-				}
-			}
-			if (flag)
+
+			if (validate_2(i, j))
 				continue;
 
 			printf("#");
 		}
 		printf("\n");
 	}
+}
+
+/**
+ * validate_1 - Menger sponge
+ *
+ * @pot_a: level menger
+ * @i: integer column
+ * @j: integer row
+ * Return: EXIT_SUCCESS or EXIT_FAILURE
+ */
+int validate_1(int pot_a, int i, int j)
+{
+	int k, l;
+
+	for (k = 1; k < pot_a / 3; k += 3)
+	{
+		for (l = 1; l < pot_a / 3; l += 3)
+		{
+			if ((pot_a / 9) * k <= i && ((pot_a / 9) * k) + pot_a / 9 > i)
+			{
+				if ((pot_a / 9) * l <= j && ((pot_a / 9) * l) + pot_a / 9 > j)
+				{
+					printf(" ");
+					return (1);
+				}
+			}
+		}
+	}
+	return (0);
+}
+
+/**
+ * validate_2 - Menger sponge
+ *
+ * @i: integer column
+ * @j: integer row
+ * Return: EXIT_SUCCESS or EXIT_FAILURE
+ */
+int validate_2(int i, int j)
+{
+	int k, l;
+
+	for (k = 3; k < 6; k++)
+	{
+		for (l = 3; l < 6; l++)
+		{
+			if (i % 9 == k && j % 9 == l)
+			{
+				printf(" ");
+				return (1);
+			}
+		}
+	}
+	return (0);
 }
--- 0x0B-menger/menger.h
+++ 0x0B-menger/menger.h
@@ -4,5 +4,6 @@
 #include <math.h>
 #include <stdio.h>
 void menger(int level);
-
+int validate_1(int pot_a, int i, int j);
+int validate_2(int i, int j);
 #endif
