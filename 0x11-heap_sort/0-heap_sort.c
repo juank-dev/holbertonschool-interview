a926881b24e25a62e8649d3b9d598f7bc302a2fb
Juan <1213@holbertonschool.com>
2021-04-26 20:06:18 -0500

    Heap Sort Project

--- 0x11-heap_sort/0-heap_sort.c
+++ 0x11-heap_sort/0-heap_sort.c
@@ -6,9 +6,9 @@
  */
 void _swap(int *i, int *j)
 {
-    int tmp = *i;
-    *i = *j;
-    *j = tmp;
+	int tmp = *i;
+	*i = *j;
+	*j = tmp;
 }
 /**
  * build_heap - builds heap out of list
@@ -21,22 +21,22 @@ void _swap(int *i, int *j)
 
 void build_heap(int *list_heap, int i, int j, size_t size)
 {
-    int max = j;
-    int left = j * 2 + 1;
-    int right = j * 2 + 2;
+	int max = j;
+	int left = j * 2 + 1;
+	int right = j * 2 + 2;
 
-    if (left < i && list_heap[left] > list_heap[max])
-        max = left;
+	if (left < i && list_heap[left] > list_heap[max])
+		max = left;
 
-    if (right < i && list_heap[right] > list_heap[max])
-        max = right;
+	if (right < i && list_heap[right] > list_heap[max])
+		max = right;
 
-    if (max != j)
-    {
-        _swap(&list_heap[j], &list_heap[max]);
-        print_array(list_heap, size);
-        build_heap(list_heap, i, max, size);
-    }
+	if (max != j)
+	{
+		_swap(&list_heap[j], &list_heap[max]);
+		print_array(list_heap, size);
+		build_heap(list_heap, i, max, size);
+	}
 }
 /**
  * heap_sort - sorts an array of integers in ascending order
@@ -48,16 +48,16 @@ void build_heap(int *list_heap, int i, int j, size_t size)
 
 void heap_sort(int *list_heap, size_t size)
 {
-    int i;
+	int i;
 
-    for (i = size / 2 - 1; i >= 0; i--)
-        build_heap(list_heap, size, i, size);
+	for (i = size / 2 - 1; i >= 0; i--)
+		build_heap(list_heap, size, i, size);
 
-    for (i = size - 1; i >= 0; i--)
-    {
-        _swap(&list_heap[0], &list_heap[i]);
-        if (i != 0)
-            print_array(list_heap, size);
-        build_heap(list_heap, i, 0, size);
-    }
+	for (i = size - 1; i >= 0; i--)
+	{
+		_swap(&list_heap[0], &list_heap[i]);
+		if (i != 0)
+			print_array(list_heap, size);
+		build_heap(list_heap, i, 0, size);
+	}
 }
