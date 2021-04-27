4d9b51c32695c18f174089fcc2862e73ba23ed34
Juan <1213@holbertonschool.com>
2021-04-26 19:11:21 -0500

    Correction rain project

--- 0x10-rain/0-rain.py
+++ 0x10-rain/0-rain.py
@@ -3,40 +3,32 @@
 Project: Rain
 """
 
-
 def validation(new_list: list) -> list:
     """Validate a list"""
-    count = False
+    count = 0
     first_list = []
     for x in new_list:
-        if x <= 0 and count is False:
-            continue
-        first_list.append(x)
-        count = True
-    
-    count = False
+        if x > 0 or count != 0:
+            first_list.append(x)
+            count += 1
+    count = 0
     second_list = []
     for y in reversed(first_list):
-        if x <= 0 and count is False:
-            continue
-        second_list.append(x)
-        count = True
+        if y > 0 or count != 0:
+            second_list.append(y)
+            count += 1
     return second_list
 
-
 def rain(walls):
-    """Function: Calculate how much water will be retained
+    """Function: calculate how much water will be retained
     after it rains.
     """
-    if len(walls) == 0:
-        return 0
-
     count = 0
     maximo = max(walls)
     rain = 0
     for x in range(maximo):
         new_list = validation(walls)
-
+        
         for w in new_list:
             if w <= 0:
                 rain += 1
---
+++ 0x10-rain/0_main.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+0_main
+"""
+rain = __import__('0-rain').rain
+
+if __name__ == "__main__":
+    walls = [2, 0, 0, 0, 0, 0, 0, 1]
+    print(rain(walls))
+    walls = [0, 1, 0, 2, 0, 3, 0, 4]
+    print(rain(walls))
+    walls = [0, 1, 0, 2, 0, 1, 0, 4]
+    print(rain(walls))
+    walls = [0, 4, 0, 2, 3, 1, 0, 0]
+    print(rain(walls))
+    walls = [1, 0, 4, 0, 2, 3, 0, 1]
+    print(rain(walls))
+    walls = [2, 0, 0, 4, 0, 0, 1, 0]
+    print(rain(walls))
